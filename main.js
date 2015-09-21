a77edeb8b888d8ba96a4647905b2150f04d04464
Tanay Gaherwar <tanay_tgs@yahoo.co.in>
2015-09-21 22:01:02 +0530

    Update main.js
    Tried db access

--- main.js
+++ main.js
@@ -31,8 +31,27 @@ Parse.Cloud.define( "PrintThis", function(request, response){
 	// ================================
 	// WORKING EXAMPLE 2
 	// ================================
+	/*
 	var sum = request.params._ele1 + 		request.params._ele2;
 	response.success({
 		"sum" : sum
 	});
+	*/
+	
+	// ====
+        var GameScore = Parse.Object.extend("GameScore");
+        console.log("test1");
+        var query = new Parse.Query("GameScore");
+        query.equalTo("playerName", "Sean Plott");
+        query.count({
+        success: function(number) {
+          response.success(
+            { "_numberOfSeans":number }
+          );
+        },
+	error: function(error) {
+          response.error(
+            { "reply":"bitches" }
+          );
+      	}
 });
