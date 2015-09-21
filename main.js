70bfc45d44f91e789031c9c798ec9e31b8fab924
Tanay Gaherwar <tanay_tgs@yahoo.co.in>
2015-09-21 20:49:20 +0530

    commit cloud sum code

---
+++ main.js
@@ -0,0 +1,38 @@
+
+Parse.Cloud.define( "PrintThis", function(request, response){
+
+	// ================================
+	// NOT WORKING YET
+	// ================================
+	/*
+		var GameScore = Parse.Object.extend("GameScore");
+		console.log("test1");
+		var query = new Parse.Query("GameScore");
+		query.equalTo("playerName", "Sean Plott" );
+		query.count({
+	  success: function(number) {
+	    response.success(number);
+	  },
+	  error: function(error) {
+			response.error("bitches");
+	  }
+ });
+ */
+
+	// ================================
+	// WORKING EXAMPLE 1
+	// ================================
+	/*
+ 	response.success({
+		"reply" : request.params._message
+	});
+	*/
+
+	// ================================
+	// WORKING EXAMPLE 2
+	// ================================
+	var sum = request.params._ele1 + 		request.params._ele2;
+	response.success({
+		"sum" : sum
+	});
+});
