a2b8d1194ece99a0e496398fd6f229180c512248
Tanay Gaherwar <tanay_tgs@yahoo.co.in>
2015-10-02 11:54:04 +0530

    Proper plug-in cloud Code
    working, for updating EndUser

--- main.js
+++ main.js
@@ -1,57 +1,33 @@
 
-Parse.Cloud.define( "PrintThis", function(request, response){
 
-	// ================================
-	// NOT WORKING YET
-	// ================================
-	/*
-		var GameScore = Parse.Object.extend("GameScore");
-		console.log("test1");
-		var query = new Parse.Query("GameScore");
-		query.equalTo("playerName", "Sean Plott" );
-		query.count({
-	  success: function(number) {
-	    response.success(number);
-	  },
-	  error: function(error) {
-			response.error("bitches");
-	  }
- });
- */
+Parse.Cloud.define( "EndPlayerSessionStartedEvent", function(request, response){
 
-	// ================================
-	// WORKING EXAMPLE 1
-	// ================================
-	/*
- 	response.success({
-		"reply" : request.params._message
-	});
-	*/
-
-	// ================================
-	// WORKING EXAMPLE 2
-	// ================================
-	/*
-	var sum = request.params._ele1 + 		request.params._ele2;
-	response.success({
-		"sum" : sum
-	});
-	*/
-	
-	// ====
-        var GameScore = Parse.Object.extend("GameScore");
-        console.log("test1");
-        var query = new Parse.Query("GameScore");
-        query.equalTo("playerName", "Sean Plott");
-        query.count({
-        success: function(number) {
-          response.success(
-            { "_numberOfSeans":number }
-          );
-        },
-	error: function(error) {
-          response.error(
-            { "reply":"bitches" }
-          );
-      	}
-});
+       var EndPlayer = Parse.Object.extend("EndPlayer");
+        var meid;
+        meid=request.params._meid;
+        query = new Parse.Query("EndPlayer");
+        query.equalTo("MEID",meid );
+        query.find({
+          success: function(results)
+          {
+            if ( results.length == 0 )
+            {
+              // new meid, add to db
+              var endPlayer= new EndPlayer();
+              endPlayer.set("MEID",meid1+i.toString());
+              endPlayer.save(null,{
+                success: function(object) {}
+              });
+              response.success({"response":"new meid"});
+            }
+            else if ( results.length == 1)
+            {
+              //mewid already present
+                response.success({"response":"meid already present"});
+            }
+          },
+          error: function( error){
+            response.error({"response":"some error occured"});
+          }
+        });
+      });
